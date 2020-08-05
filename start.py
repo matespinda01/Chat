6ef275253a22377bf2400e724e92ec44b313569f
matespinda01 <65965744+matespinda01@users.noreply.github.com>
2020-08-05 19:13:58 +0200

    CLI

---
+++ start.py
@@ -0,0 +1,17 @@
+'''
+ # @ Project: Chat
+ # @ Author: Spinda Máté
+ # @ Create Time: 2020-08-05 18:51:15
+ # @ Email: mate.spinda01@gmail.com
+ # @ GitHub: https://github.com/matespinda01/Chat
+ # @ Licence: MIT
+
+ # @ Description: Starting
+
+
+ '''
+
+import curses
+import sys
+
+
